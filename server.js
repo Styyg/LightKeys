72ef3ef1fc3875a20844882ccda2e55db2a26d2f
Laurent Hirth <laurent.hirth.68@gmail.com>
2021-08-06 21:36:09 +0200

    no refresh after submit

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-const fs = require("fs");
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+const fs = require('fs')
 
 // Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static("public"));
+app.use(express.static('public'))
 
-app.get("/", function (req, res) {
-  res.sendFile('index.html');
-});
+app.get('/', function (req, res) {
+	res.sendFile('index.html')
+})
 
-app.post("/process_post", urlencodedParser, function (req, res) {
-  // Prepare output in JSON format
-  response = {
-    colorRGB: req.body.colorRGB,
-    colorW: req.body.colorW,
-    brightness: req.body.brightness,
-  };
-  fs.writeFileSync(
-    __dirname + "/public/settings.json",
-    JSON.stringify(response)
-  );
-  res.redirect("back");
-});
+app.post('/process_post', urlencodedParser, function (req, res) {
+	// Prepare output in JSON format
+	output = {
+		colorRGB: req.body.colorRGB,
+		colorW: req.body.colorW,
+		brightness: req.body.brightness,
+	}
+	fs.writeFileSync(__dirname + '/public/settings.json', JSON.stringify(output))
+})
 
 var server = app.listen(8081, function () {
-  var port = server.address().port;
+	var port = server.address().port
 
-  console.log("App listening at http://localhost:%s", port);
-});
+	console.log('App listening at http://localhost:%s', port)
+})
