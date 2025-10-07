6bb83413f87686ed2cf2dfb3f62e41a257e9d182
Styg <matthieu.hirth.68@gmail.com>
2025-10-07 20:47:20 +0200

    ADD: Effects modes

---
+++ effect_modes.py
@@ -0,0 +1,90 @@
+from abc import ABC, abstractmethod
+import time
+import logging
+
+log = logging.getLogger("EFFECT")
+
+class EffectMode(ABC):
+    def __init__(self):
+        self.notes_to_render = {}
+
+    @abstractmethod
+    def apply(self, notes_state: dict, frame_note_events: list) -> list:
+        pass
+
+    def get_notes_to_render(self):
+        return self.notes_to_render
+
+class NoteOnEffect(EffectMode):
+    def __init__(self):
+        super().__init__()
+    
+    def apply(self, notes_state: dict, frame_note_events: list) -> list:
+        self.notes_to_render = {}
+        for note_key in frame_note_events:
+            note_data = notes_state.get(note_key, {})
+            state = note_data["state"]
+
+            if state == 1:  # note pressed
+                color = note_data.get("color", (0, 0, 255, 0))
+                self.notes_to_render[note_key] = color
+            else:
+                self.notes_to_render[note_key] = (0, 0, 0, 0)
+
+class FadeOutEffect(EffectMode):
+    def __init__(self, fade_time=0.5):
+        super().__init__()
+        self.fade_time = fade_time
+        self.fading_notes = {}
+
+    def apply(self, notes_state: dict, frame_note_events: list) -> list:
+        now = time.monotonic()
+        self._cleanup_notes_to_render()
+
+        # add new notes to fading_notes
+        for note_key in frame_note_events:
+            note_data = notes_state.get(note_key, {})
+            state = note_data["state"]
+
+            if state == 1:  # note pressed
+                continue
+
+            start_time = note_data["start_time"]
+            color = note_data.get("color", (0, 0, 255, 0))
+
+            # note released
+            self.fading_notes[note_key] = {
+                "start_time": start_time,
+                "end_time": start_time + self.fade_time,
+                "fade_value": 1.0,
+                "color": color
+            }
+
+        # update fading notes
+        fade_to_delete = []
+        for note_key, note_data in self.fading_notes.items():
+            elapsed = now - note_data["start_time"]
+            factor = max(0, 1 - elapsed / self.fade_time)
+            if factor <= 0:
+                fade_to_delete.append(note_key)
+                continue
+
+            r, g, b, w = note_data["color"]
+            self.notes_to_render[note_key] = (
+                int(r * factor), 
+                int(g * factor), 
+                int(b * factor), 
+                int(w * factor))
+            
+        for note in fade_to_delete:
+            self.fading_notes.pop(note, None)
+            self.notes_to_render[note] = (0, 0, 0, 0)
+    
+    def _cleanup_notes_to_render(self):
+        notes_to_delete = []
+        for note, color in self.notes_to_render.items():
+            if color == (0, 0, 0, 0):
+                notes_to_delete.append(note)
+        
+        for note in notes_to_delete:
+            del self.notes_to_render[note]
--- renderer.py
+++ renderer.py
@@ -19,7 +19,13 @@ class Renderer:
         self.queue = queue.Queue()   # queue partagée avec le MidiListener
         self.strip = LEDStrip()  # instance du bandeau
         self.running = False
-        self.colorMode = color_modes.OneColor((0,128,128,0))
+        self.noteToLeds = {}  # mapping note → LED index
+        self.notes_state = {}
+        self.colorMode = None
+        self.effectMode = None
+        self.effectsMode = []
+        
+        # self.set_color_mode(color_modes.OneColor((255,0,0,0)))
 
         # dictionary = {21: (255, 0, 0, 0), 
         #              108: (255, 255, 255, 0)
@@ -27,10 +33,11 @@ class Renderer:
         # gradient = color_modes.Gradient(dictionary)
         # self.set_color_mode(gradient)
 
-        self.set_color_mode(color_modes.VelocityBased((255,0,0,0), (0,255,0,0), 40, 100))
+        self.set_color_mode(color_modes.VelocityBased((255,0,0,0), (0,0,0,255), 40, 100, color_modes.ease_in_power))
 
-        # self.effectMode = effect_modes
-        self.noteToLeds = {}  # mapping note → LED index
+        self.add_effect_mode(effect_modes.NoteOnEffect())
+        self.add_effect_mode(effect_modes.FadeOutEffect(0.2))
+
         self.initNotesToLeds()
 
     def initNotesToLeds(self):
@@ -47,45 +54,73 @@ class Renderer:
                 case _:
                     self.noteToLeds[note] = {led_number, led_number + 1}
                     led_number += 2
-        # print("Note to LED mapping:", self.noteToLeds)
 
     def start(self):
         """Boucle principale du rendu"""
         self.running = True
         frame_time = 1.0 / FPS
         next_frame = time.monotonic()
+        note_events_this_frame = []
 
         while self.running:
             while not self.queue.empty():
                 msg = self.queue.get_nowait()
-                led_indices = []
                 if msg.type in ("note_on", "note_off"):
                     if msg.note < LOWEST_MIDI_NOTE or msg.note > HIGHEST_MIDI_NOTE:
                         log.warning(f"Note {msg.note} out of range ({LOWEST_MIDI_NOTE}-{HIGHEST_MIDI_NOTE})")
                         continue
-                    led_indices = self.noteToLeds.get(msg.note, [])
+
                 match msg.type:
                     case "note_on":
-                        # On allume la LED correspondante en rouge
-                        for led_index in led_indices:
-                            self.strip.set_led(led_index, self.colorMode.get_color(msg.note, msg.velocity))
-                    case "note_off":
-                        # On éteint la LED correspondante
-                        for led_index in led_indices:
-                            self.strip.set_led(led_index, (0, 0, 0, 0))
+                        color = self.colorMode.get_color(msg.note, msg.velocity)
+
+                        if msg.note not in self.notes_state:
+                            self.notes_state[msg.note] = {}
+                        self.notes_state[msg.note].update({
+                            "state": 1,
+                            "velocity": msg.velocity,
+                            "start_time": time.monotonic(),
+                            "color": color
+                        })
+                        note_events_this_frame.append(msg.note)
+                        
+                    case "note_off":                        
+                        if msg.note not in self.notes_state:
+                            self.notes_state[msg.note] = {}
+                        self.notes_state[msg.note].update({
+                            "state": 0,
+                            "velocity": msg.velocity,
+                            "start_time": time.monotonic()
+                        })
+                        note_events_this_frame.append(msg.note)
+
                     case "control_change":
                         pass
-                        # log.debug(f"Control change: {msg.control}, {msg.value}")
                     case _:
                         log.warning(f"Unhandled event: {str(msg)}")
+
 
             # On rafraîchit le ruban à chaque frame
             now = time.monotonic()
             if now >= next_frame:
+                self.render_effects(note_events_this_frame)
+                note_events_this_frame.clear()
                 self.strip.show()
                 next_frame = now + frame_time
 
         time.sleep(0.001)
+
+    def render_effects(self, note_events):
+        # self.effectMode.apply(self.notes_state)
+        notes_to_render = {}        
+        for effect in self.effectsMode:
+            effect.apply(self.notes_state, note_events)
+            notes_to_render.update(effect.get_notes_to_render())
+
+        for note, color in notes_to_render.items():
+            led_indices = self.noteToLeds.get(note, [])
+            for led_index in led_indices:
+                self.strip.set_led(led_index, color)
 
     def stop(self):
         """Arrêt propre du rendu"""
@@ -98,3 +133,8 @@ class Renderer:
         """Change le mode de couleur"""
         self.colorMode = mode
         log.info(f"Color mode changed to {type(mode).__name__}")
+
+    def add_effect_mode(self, mode: effect_modes.EffectMode):
+        """Ajoute un mode d'effet (en plus de l'actuel)"""
+        self.effectsMode.append(mode)
+        log.info(f"Effect mode added: {type(mode).__name__}")
